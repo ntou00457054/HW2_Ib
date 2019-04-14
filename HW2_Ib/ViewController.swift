e25d2723343139e0b652c0c67ada913dcc8f17b6
“Your <goodjob132@yahoo.com.tw>
2019-04-14 15:01:01 +0800

    Initial Commit

---
+++ HW2_Ib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2_Ib
+//
+//  Created by User03 on 2019/4/14.
+//  Copyright © 2019 User03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
